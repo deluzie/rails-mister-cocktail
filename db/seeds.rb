11914e352e8ddccae3b05708092148d375837bfb
Luzie Deubel <luzie@deubels.de>
2020-11-12 17:00:36 +0100

    implementing the actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'deleting Records'
+Dose.delete_all
+Ingredient.delete_all
+Cocktail.delete_all
+
+puts 'creating Ingredients'
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "vermouth")
+Ingredient.create(name: "seltzer")
+Ingredient.create(name: "dark rum")
+Ingredient.create(name: "campari")
+Ingredient.create(name: "orange bitter")
+
+puts 'finished'
