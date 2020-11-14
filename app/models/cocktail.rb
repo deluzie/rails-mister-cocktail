d1b0be93c55cd9d31cd6220615f0cdab94981b43
Luzie Deubel <luzie@deubels.de>
2020-11-14 20:12:14 +0100

    styling updates

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,7 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  has_one_attatched :photo
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :name, uniqueness: true
